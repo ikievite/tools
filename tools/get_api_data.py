d83ca626975ebe80dc596902f3189dc39900e8eb
ikievite <ikievite@gmail.com>
2022-12-02 11:55:54 +0200

    add `get_data` func

---
+++ tools/get_api_data.py
@@ -0,0 +1,22 @@
+import logging
+
+import requests
+from requests.exceptions import HTTPError
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response
+    except HTTPError as http_err:
+        logger.critical(f"HTTP error occurred: {http_err}")
+    except Exception as exc:
+        logger.critical(f"Other error occurred: {exc}")
+
+
+if __name__ == "__main__":
+    print(get_data("https://randomuser.me/api/").json())
