4413cf46c213067786c6dddeae71ce05cb8c57a2
ikievite <ikievite@gmail.com>
2022-12-03 10:12:02 +0200

    upate tests

--- tests/test_intf_vlans.py
+++ tests/test_intf_vlans.py
@@ -1,8 +1,22 @@
+
+
+"""Tests for int_vlan module."""
+
+import pytest
+
 from tools.intf_vlans import parse_rc_intf
 
-
-def test_parse_rc_intf():
-    data = """
+trunk_intf = """
+!
+interface port-channel 1
+description Po1
+switchport trunk allowed vlan 7,14,16,19,33,35-38 confirm
+switchport trunk allowed vlan add 1915,2019,2030
+switchport mode trunk
+switchport reject-frame untagged
+!
+"""
+acceess_intf = """
 !
 interface gigaethernet 1/1/1
 description subs-34161
@@ -11,5 +25,21 @@ storm-control unknown-multicast pps 1024
 storm-control dlf pps 1024
 !
 """
-    expected = {'gigaethernet 1/1/1': {'mode': 'access', 'vlans': [2009]}}
-    assert parse_rc_intf(data) == expected
+
+testdata = [
+    (
+        trunk_intf,
+        {
+            "port-channel 1": {
+                "mode": "trunk",
+                "vlans": [7, 14, 16, 19, 33, 35, 36, 37, 38, 1915, 2019, 2030],
+            },
+        },
+    ),
+    (acceess_intf, {"gigaethernet 1/1/1": {"mode": "access", "vlans": [2009]}}),
+]
+
+
+@pytest.mark.parametrize("cfg_intf, expected", testdata)
+def test_parse_rc_intf(cfg_intf, expected):
+    assert parse_rc_intf(cfg_intf) == expected
