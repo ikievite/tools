c082a373e5b4810ea9a0a8738466448b65a275cd
ikievite <ikievite@gmail.com>
2022-12-29 11:02:20 +0200

    start working on `find_auth_servers` func

---
+++ tests/test_parse.py
@@ -0,0 +1,70 @@
+"""Tests for parse module."""
+
+
+from tools.parse import find_auth_servers
+
+
+"""
+DGS-3200-10:4#show authen server_host      
+Command: show authen server_host
+
+IP Address       Protocol  Port   Timeout  Retransmit  Key
+---------------  --------  -----  -------  ----------  -------------------------
+94.232.215.29    RADIUS    1812   2        2           1qazZAQ!                 
+
+Total Entries : 1
+
+
+DGS-3000-10TC:admin#show authen server_host      
+Command: show authen server_host
+
+IP Address           Protocol Port  Timeout Retransmit Key                    
+-------------------- -------- ----- ------- ---------- -----------------------
+94.232.215.29        RADIUS   1812  2       2          1qazZAQ!               
+100.64.0.10          RADIUS   1812  2       2          1qazZAQ!               
+
+
+Total Entries : 2
+
+DES-3526:admin#show authen server_host     
+Command: show authen server_host
+
+IP Address       Protocol  Port   Timeout  Retransmit  Key
+---------------  --------  -----  -------  ----------  -------------------------
+94.232.215.29    RADIUS    1812   2        2           1qazZAQ!                 
+192.168.254.10   RADIUS    1812   2        2           1qazZAQ!                 
+
+Total Entries : 2
+
+DES-3028:5#show authen server_host      
+Command: show authen server_host
+
+IP Address       Protocol  Port   Timeout  Retransmit  Key
+---------------  --------  -----  -------  ----------  -------------------------
+94.232.215.29    RADIUS    1812   2        2           1qazZAQ!                 
+
+Total Entries : 1
+
+DES-3200-26:admin#show authen server_host      
+Command: show authen server_host
+
+IP Address           Protocol Port  Timeout Retransmit Key                    
+-------------------- -------- ----- ------- ---------- -----------------------
+94.232.215.29        RADIUS   1812  2       2          1qazZAQ!               
+
+
+Total Entries : 1
+"""
+
+def test_find_auth_servers():
+    cfg = """DGS-3200-10:4#show authen server_host      
+Command: show authen server_host
+
+IP Address       Protocol  Port   Timeout  Retransmit  Key
+---------------  --------  -----  -------  ----------  -------------------------
+94.232.215.29    RADIUS    1812   2        2           1qazZAQ!                 
+
+Total Entries : 1
+"""
+    expected = ["94.232.215.29"]
+    assert find_auth_servers(cfg) == expected
---
+++ tools/parse.py
@@ -0,0 +1,28 @@
+"""parse"""
+
+
+from ttp import ttp
+
+
+ttp_template = """
+{{ ip_addr }} {{ proto }} {{ port }} {{ timeout }} {{ retransmit }} {{ key }}
+"""
+
+cfg = """DGS-3200-10:4#show authen server_host      
+Command: show authen server_host
+
+IP Address       Protocol  Port   Timeout  Retransmit  Key
+---------------  --------  -----  -------  ----------  -------------------------
+94.232.215.29    RADIUS    1812   2        2           1qazZAQ!                 
+
+Total Entries : 1"""
+
+
+def find_auth_servers(config: str) -> list[str, ...]:
+    parser = ttp(data=config, template=ttp_template)
+    parser.parse()
+    return parser.result(format="json")[0]
+
+
+if __name__ == "__main__":
+    print(find_auth_servers(cfg))
