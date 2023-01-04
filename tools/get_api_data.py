d9d7263b7989e549be3f72c188e098cb65c330b0
ikievite <ikievite@gmail.com>
2023-01-04 16:04:34 +0200

    add func `get_all_data`

--- tools/get_api_data.py
+++ tools/get_api_data.py
@@ -1,7 +1,10 @@
 """Get API data."""
 
+import itertools
 import logging
 import os
+from pprint import pprint
+from typing import Iterator, Optional
 
 import requests
 from dotenv import load_dotenv
@@ -11,27 +14,54 @@ load_dotenv()
 
 logger = logging.getLogger(__name__)
 
-API_MODELS_URLS = os.getenv("API_MODELS_UPL")
+API_MODELS_URL = os.getenv("API_MODELS_URL")
+API_SWITCHES_URL = os.getenv("API_SWITCHES_URL")
 
 
-def get_data(url: str) -> requests.Response:
+def get_data(url: str, payload: Optional[dict[str, int]] = None):
     """Get API data.
 
     Args:
         url: url
+        payload: payload
 
     Returns:
-        response
+        request response
     """
     try:
-        response = requests.get(url)
+        response = requests.get(url, params=payload)
         response.raise_for_status()
-        return response
+        return response.json()
     except HTTPError as http_err:
         logger.critical(f"HTTP error occurred: {http_err}")
     except Exception as exc:
         logger.critical(f"Other error occurred: {exc}")
 
 
+def get_all_data(
+    url: str,
+    meta_field: str = "meta",
+    data_field: str = "data",
+) -> Iterator[dict[str, str]]:
+    """Get data with API pagination.
+
+    Args:
+        url: url
+        meta_field: meta field name
+        data_field: data field name
+
+    Yields:
+        list with items
+    """
+    response = get_data(url)
+    yield response[data_field]
+    last_page = response.get(meta_field).get("pages")
+    for page in range(2, last_page + 1):
+        payload = {"page": page}
+        yield get_data(url, payload=payload)[data_field]
+
+
 if __name__ == "__main__":
-    print(get_data(API_MODELS_URLS).json())  # noqa:  WPS421
+    switches = get_all_data(API_MODELS_URL)
+    pprint(list(itertools.chain(*switches)))  # noqa: WPS421
+    # print(get_data(API_SWITCHES_URL).json())  # noqa:  WPS421
